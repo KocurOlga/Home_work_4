2345256d1a45e98fd8d25a947723250a1b53edcb
Kocur Olga <olkocur@gmail.com>
2020-10-29 17:48:02 +0700

    Full programm popular item title

--- constants.py
+++ constants.py
@@ -2,19 +2,56 @@
 GENDER_MALE = 'm'
 GENDER_FEMALE = 'f'
 
-user_list = [{'name': 'Ivan', 'gender': GENDER_MALE},
-             {'name': 'Olga', 'gender': GENDER_FEMALE},
-             {'name': 'Anna', 'gender': GENDER_FEMALE},
-             {'name': 'Petr', 'gender': GENDER_MALE}]
+#список пользователей
+user_list = [{'name': 'Иван', 'gender': GENDER_MALE},
+             {'name': 'Петр', 'gender': GENDER_MALE},
+             {'name': 'Марья', 'gender': GENDER_FEMALE},
+             {'name': 'Дарья', 'gender': GENDER_FEMALE},
+             {'name': 'Юлия', 'gender': GENDER_FEMALE}]
 
-male_count = 0
-female_count = 0
+#список товаров
+item_list = [{'title': 'Часы', 'cost': 9800},
+             {'title': 'Кофемашина', 'cost': 23500},
+             {'title': 'Фитнес-браслет', 'cost': 13200},
+             {'title': 'Айфон', 'cost': 73900},
+             {'title': 'Чехол для телефона', 'cost': 250}]
 
-for user in user_list:
-    if user['gender'] == GENDER_MALE:
-        male_count += 1
-    else:
-        female_count += 1
+#журнал регистрации какой пользователь что купил
+log = [{'user': user_list[0], 'purchases': [item_list[0], item_list[1], item_list[2]]},
+       {'user': user_list[1], 'purchases': [item_list[0], item_list[2]]},
+       {'user': user_list[2], 'purchases': [item_list[2], item_list[3]]},
+       {'user': user_list[3], 'purchases': [item_list[2], item_list[3]]},
+       {'user': user_list[4], 'purchases': [item_list[4], item_list[2]]}]
 
-print('Мужчин в списке', male_count)
-print('Женщин в списке', female_count)
+# Создадим список для хранения популярных товар: popular_items.
+popular_items = []
+
+# Обходим все записи из списка log
+for record in log:
+    # Для каждой записи из списка 'log' в цикле обходим все записи из списка 'purchases'
+    for item in record['purchases']:
+        # Получаем название купленного товара
+        purchase_title = item['title']
+        # Ищем купленный товар в списке popular_items по ключу 'title'
+        found = False
+        for popular_item in popular_items:
+            if popular_item['title'] == purchase_title:
+                popular_item['quantity'] += 1
+                found = True
+                break
+        # Если купленный товар не найден в списке popular_items,
+        # то добавляем его и ставим количество (quantity) = 1
+        if not found:
+            popular_items.append({'title': purchase_title, 'quantity': 1})
+# Выводим продажи по каждому товару
+for popular_item in popular_items:
+    print(f"Количество продаж товара {popular_item['title']} = {popular_item['quantity']}")
+
+#определяем максимальной количество продаж
+max_count = 0
+popular_item_title = ''
+for quant in popular_items:
+    if max_count < quant['quantity']:
+        max_count = quant['quantity']
+        popular_item_title = quant['title']
+print(f'Самый популярный товар {popular_item_title} с числом продаж {max_count}')
